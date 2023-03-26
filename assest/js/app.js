2c6938f160e8b4d4b46870b37b678bf5fbd275c8
web-vikas <111357844+web-vikas@users.noreply.github.com>
2023-03-26 19:25:45 +0530

    Add files via upload

--- assest/js/app.js
+++ assest/js/app.js
@@ -1,10 +1,15 @@
 const hamburger = document.querySelector('#hamburger')
 const mobilemenu = document.querySelector('#mobilemenu')
 const closeMenu = document.querySelector('#closeMenu')
+const backtopBtn = document.querySelector('#backtop')
 
 hamburger.addEventListener('click',()=>{
     mobilemenu.classList.remove('right')
 })
 closeMenu.addEventListener('click',()=>{
     mobilemenu.classList.add('right')
+})
+backtopBtn.addEventListener('click',()=>{
+    window.scrollTo(0,0)
+    
 })
